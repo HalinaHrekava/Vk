e6abefca43b29facc2bbe6e93bb9b0ccccb99a7a
Halina Hrekava <gluk2019@mail.ru>
2021-05-03 17:01:25 +0300

    PostManager added

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -4,7 +4,9 @@ import ru.netology.domain.Post;
 
 public class PostManager {
     private Post[] posts;
-    public Post[] generateBlock() {
+    public Post[] wallSearch(int ownerId, String domain, String query, boolean ownersOnly, byte count, int offset) {
         return null;
+    }
+    public void delete(int ownerID, int postID) {
     }
 }
